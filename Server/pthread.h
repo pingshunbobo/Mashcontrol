a09a05b673e91bda77f7afcc481cde6873284966
bobo <bobo@pingshun.(none)>
2016-07-21 19:17:36 +0800

    new file:   Server/Makefile 	new file:   Server/a.out 	new file:   Server/pr_cpu_time.c 	new file:   Server/pthread.c 	new file:   Server/pthread.h 	new file:   Server/serv.c 	new file:   Server/tcp_listen.c 	new file:   Server/unp.h 	new file:   Server/unpthread.h 	new file:   Server/web_child.c

---
+++ Server/pthread.h
@@ -0,0 +1,11 @@
+#include <pthread.h>
+#include <sys/socket.h>
+typedef struct{
+        pthread_t             thread_tid;     /* thread ID */
+        long                  thread_count;   /* #connections handled */
+} Thread;
+
+Thread  *tptr;          /* array of Thread structures; calloc'ed */
+
+int                     listenfd, nthreads;
+socklen_t               addrlen;
